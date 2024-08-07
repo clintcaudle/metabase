64e80b92b5179f406c1d539f3dc10d94c9bf4890
clint caudle <2287108+clintcaudle@users.noreply.github.com>
2024-08-07 17:27:16 -0400

    correspond icons to unit type

--- frontend/src/metabase/visualizations/components/CarinaIcons.jsx
+++ frontend/src/metabase/visualizations/components/CarinaIcons.jsx
@@ -1,56 +1,56 @@
 import L from "leaflet";
 
 export const markerIcons = {
-  1: L.icon({
+  10: L.icon({
     //"CAP"
     iconUrl: "app/assets/img/carina/capacitor.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  2: L.icon({
+  11: L.icon({
     //"CIRCB"
     iconUrl: "app/assets/img/carina/circuit-breaker.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  3: L.icon({
+  12: L.icon({
     //"FUSE"
     iconUrl: "app/assets/img/carina/fuse.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  4: L.icon({
+  13: L.icon({
     //SECTIONALIZER"
     iconUrl: "app/assets/img/carina/sectionalizer.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  5: L.icon({
+  14: L.icon({
     // "SPRECLOSER":
     iconUrl: "app/assets/img/carina/single-phase-recloser.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  6: L.icon({
+  15: L.icon({
     //"TPRECLOSER"
     iconUrl: "app/assets/img/carina/triple-phase-recloser.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  7: L.icon({
+  16: L.icon({
     //"THRESPRECLOSER"
     iconUrl: "app/assets/img/carina/three-single-phase-recloser.png",
     iconSize: [28, 32],
     iconAnchor: [15, 24],
     popupAnchor: [0, -13],
   }),
-  8: L.icon({
+  17: L.icon({
     // "TWOSPRECLOSER"
     iconUrl: "app/assets/img/carina/two-single-phase-recloser.png",
     iconSize: [28, 32],
