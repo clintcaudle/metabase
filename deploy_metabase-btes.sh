10a2eaff931fbf22290ca617043881a15b781b1f
clint caudle <2287108+clintcaudle@users.noreply.github.com>
2024-08-08 13:10:29 -0400

    update to metabase v0.50.19

--- deploy_metabase-btes.sh
+++ deploy_metabase-btes.sh
@@ -2,7 +2,7 @@
 ./bin/build.sh
 yarn install
 yarn build
-docker build -f ~/metabase/Dockerfile ~/metabase/. --tag metabase --build-arg=VERSION=v0.48.8
+docker build -f ~/metabase/Dockerfile ~/metabase/. --tag metabase --build-arg=VERSION=v0.50.19
 docker image save metabase > metabase.tar
 scp ~/metabase/metabase.tar carina@10.150.0.7:~/metabase/
 scp ~/metabase/redeploy_metabase-btes.sh carina@10.150.0.7:~/metabase/
